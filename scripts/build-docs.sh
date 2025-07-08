04fec68faab7a3e458d24c1d61508d78d33babb9
kaysee clintone <kayseeclintone@gmail.com>
2025-07-08 18:42:59 +0000

    Remove awesome-pages dependency from documentation build scripts

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Ensure mkdocs and required plugins are installed
-pip install mkdocs mkdocs-material mkdocs-pdf-export-plugin awesome-pages
+pip install mkdocs mkdocs-material mkdocs-pdf-export-plugin
 
 echo "🔧 Building documentation structure..."
 
